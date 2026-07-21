4b8431efeab1a1a96135ee243b797c04a01365ef
ankur.raj@oracle.com <ankurraj@phoenix171831.dev3sub3phx.databasede3phx.oraclevcn.com>
2026-07-21 13:13:50 +0000

    [llvm-cov] Test covered-functions export from SPI

--- clang/test/CodeGen/coverage-mapping-spi.c
+++ clang/test/CodeGen/coverage-mapping-spi.c
@@ -5,6 +5,13 @@
 // RUN:   -o %t.o %s
 // RUN: test -s %t.o
 // RUN: test -s %t.spi
+// RUN: llvm-profdata merge %S/Inputs/coverage-mapping-spi.proftext \
+// RUN:   -o %t.profdata
+// RUN: llvm-cov export %t.spi -empty-profile -format=covered-functions \
+// RUN:   | FileCheck %s --check-prefix=SPI-BASELINE
+// RUN: llvm-cov export %t.spi -coverage-only -format=covered-functions \
+// RUN:   -instr-profile=%t.profdata \
+// RUN:   | FileCheck %s --check-prefix=SPI-EXECUTION
 // RUN: rm -f %t.o %t.spi
 // RUN: not %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-obj \
 // RUN:   -DFAIL -fprofile-instrument=clang -fcoverage-mapping \
@@ -25,6 +32,18 @@
 
 // LATEST-NOT: first_only
 // LATEST: second_only
+
+// SPI-BASELINE:      covered-functions-format 3
+// SPI-BASELINE-NEXT: mode - baseline
+// SPI-BASELINE:      function - "main"
+// SPI-BASELINE-NEXT: function-id - "main" 0x0000000000000018
+// SPI-BASELINE-NEXT: entry-count - 0
+
+// SPI-EXECUTION:      covered-functions-format 3
+// SPI-EXECUTION-NEXT: mode - execution
+// SPI-EXECUTION:      function - "main"
+// SPI-EXECUTION-NEXT: function-id - "main" 0x0000000000000018
+// SPI-EXECUTION-NEXT: entry-count - 1
 
 #ifdef FAIL
 #error expected failure
